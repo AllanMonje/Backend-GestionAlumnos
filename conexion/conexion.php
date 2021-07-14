5e1f3bec056c068c9efb2aa114ce4a102e3aa6a2
QualityAssistant2 <QualityAssistant2@basic.cbihn.com>
2021-07-14 13:31:13 -0600

    Commit inicial by AMonje

---
+++ conexion/conexion.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Monje
+ * Date: 24/7/2019
+ * Time: 19:33
+ */
+
+$conexion=mysqli_connect('localhost','root','','basenegocios') or
+                    die("Problemas con la conexion");
